945c890d2e7a3125bddb7d5da3a64bc9508ad5c1
jswong65 <jswong65@gmail.com>
2017-09-18 23:32:33 -0400

    Complete the train function for CharRNN

--- Char_RNN/train.py
+++ Char_RNN/train.py
@@ -1,45 +1,64 @@
 import time
-import model
-import config
+from model import CharRNNMoel
+from config import ModelConfig
+
 import tensorflow as tf
+import numpy as np
 
 from utils import extract_char_vocab, encode_data, get_batches
 
-config = config.ModelConfig()
-model = model.CharRNNMoel(config, )
-model.build_model()
-saver = tf.train.Saver(max_to_keep=100)
 
-with tf.Session() as sess:
-	sess.run(tf.global_variables_initializer())
-    
-	# Use the line below to load a checkpoint and resume training
-	#saver.restore(sess, 'checkpoints/______.ckpt')
-	counter = 0
-	for e in range(epochs):
-    	# Train network
-    	new_state = sess.run(model.initial_state)
-    	loss = 0
-    	for x, y in get_batches(encoded, batch_size, num_steps):
-    		counter += 1
-    		start = time.time()
-    		feed = {model.inputs: x,
-    				model.targets: y,
-    				model.keep_prob: config.keep_prob,
-    				model.initial_state: new_state}
-    		
-    		batch_loss, new_state, _ = sess.run([model.loss, 
-    											model.final_state, 
-    											model.optimizer], 
-    											feed_dict=feed)
-            
-			end = time.time()
-			print('Epoch: {}/{}... '.format(e+1, epochs),
-				'Training Step: {}... '.format(counter),
-				'Training loss: {:.4f}... '.format(batch_loss),
-				'{:.4f} sec/batch'.format((end-start)))
-        
-			if (counter % save_every_n == 0):
-				saver.save(sess, "checkpoints/i{}_l{}.ckpt".format(counter, lstm_size))
-    
-	saver.save(sess, "checkpoints/i{}_l{}.ckpt".format(counter, lstm_size))
+def train():
+
+	FLAGS = ModelConfig()
+	model = CharRNNMoel(FLAGS, "train")
+	model.build_model()
+	saver = tf.train.Saver(max_to_keep=100)
+
+	with open('anna.txt', 'r') as f:
+	    text=f.read()
+	vocab = sorted(set(text))
+	vocab_to_int = {c: i for i, c in enumerate(vocab)}
+	int_to_vocab = dict(enumerate(vocab))
+	encoded = np.array([vocab_to_int[c] for c in text], dtype=np.int32)
+
+
+	with tf.Session() as sess:
+		sess.run(tf.global_variables_initializer())
+	    
+		# Use the line below to load a checkpoint and resume training
+		#saver.restore(sess, 'checkpoints/______.ckpt')
+		counter = 0
+		for e in range(FLAGS.epochs):
+			# Train network
+			new_state = sess.run(model.initial_state)
+			loss = 0
+			for x, y in get_batches(encoded, FLAGS.batch_size, FLAGS.num_steps):
+				counter += 1
+				start = time.time()
+				feed = {model.inputs: x,
+	    				model.targets: y,
+	    				model.keep_prob: FLAGS.keep_prob,
+	    				model.initial_state: new_state}
+	    		
+				batch_loss, new_state, _ = sess.run([model.loss, 
+	    											model.final_state, 
+	    											model.train_op], 
+	    											feed_dict=feed)
+	            
+				end = time.time()
+				print('Epoch: {}/{}... '.format(e+1, FLAGS.epochs),
+					'Training Step: {}... '.format(counter),
+					'Training loss: {:.4f}... '.format(batch_loss),
+					'{:.4f} sec/batch'.format((end-start)))
+	        
+				if (counter % FLAGS.save_every_n == 0):
+					saver.save(sess, "checkpoints/i{}_l{}.ckpt".format(counter, FLAGS.lstm_size))
+	    
+		saver.save(sess, "checkpoints/i{}_l{}.ckpt".format(counter, FLAGS.lstm_size))
+
+def main():
+	train()
+
+if __name__ == "__main__":
+	main()
